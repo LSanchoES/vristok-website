c8d5b4a5ebf220c5ba850ca9b8bc34749716e4ad
vristok <lsanchoaragon@gmail.com>
2021-07-06 01:54:54 +0200

    añadiendo chimpform

--- src/components/Registrate.js
+++ src/components/Registrate.js
@@ -1,55 +1,92 @@
-import React from "react";
-import Swal from 'sweetalert2';
-import { API_KEY } from "../config";
+import React, { useEffect } from "react";
+import Swal from "sweetalert2";
 
 export const Registrate = () => {
-
-	const handleSubmit = (e) =>{
-
+	const handleSubmit = (e) => {
 		e.preventDefault();
 
 		let email = e.target[0].value;
-		console.log(email)
+		console.log(email);
 
 		//Validation Alert
-		if (email.length > 10){
-		 Swal.fire({
-			title: 'Listo!',
-			text: 'Ahora recibirás todas mis notificaciones!',
-			icon: 'success',
-			confirmButtonText: 'Pues vale :D'
-		  })}else{
+		if (email.length > 10) {
 			Swal.fire({
-				title: 'Introduce un mail real anda crack',
-				text: 'No es tan dificil...',
-				icon: 'error',
-				confirmButtonText: 'Vale ya pongo el real'
-			  })  
-		  }
+				title: "Listo!",
+				text: "Ahora recibirás todas mis notificaciones!",
+				icon: "success",
+				confirmButtonText: "Pues vale :D",
+			});
+		} else {
+			Swal.fire({
+				title: "Introduce un mail real anda crack",
+				text: "No es tan dificil...",
+				icon: "error",
+				confirmButtonText: "Vale ya pongo el real",
+			});
+		}
 
-		  //DB send mail
-
-	}
+		//DB send mail
+	};
 	return (
 		<div className="registrate__container">
-
 			<h1 id="registrate-index">
-				Introduce tu email y recibe todas las notificaciones!
+				Registra tu email para recibir todas las notificaciones!
 			</h1>
-
-			<div className="registrate__form">
-
-			<form onSubmit={handleSubmit}>
-
-				<label htmlFor="email">Email</label>
-				<input
-					type="email"
-					id="email"
-					name="email"
-					placeholder="ejemplo@gmail.com"
-				/>
-				<input type="submit" value="Notificame!" />
-			</form>
+			<div id="mc_embed_signup">
+				<form
+					action="https://lsancho.us6.list-manage.com/subscribe/post?u=71a58a6e738704a94ffc2ab89&id=cfcab31bbd"
+					method="post"
+					id="mc-embedded-subscribe-form"
+					name="mc-embedded-subscribe-form"
+					className="validate"
+					target="_blank"
+					noValidate
+					onSubmit={handleSubmit}
+				>
+					<div id="mc_embed_signup_scroll">
+						<div className="mc-field-group">
+							<label htmlFor="mce-EMAIL"></label>
+							<input
+								type="email"
+								placeholder="email@email.com"
+								name="EMAIL"
+								className="required email registrate__input"
+								id="mce-EMAIL"
+							/>
+						</div>
+						<div id="mce-responses" className="clear">
+							<div
+								className="response"
+								id="mce-error-response"
+								style={{ display: "none" }}
+							/>
+							<div
+								className="response"
+								id="mce-success-response"
+								style={{ display: "none" }}
+							/>
+						</div>{" "}
+						{/* real people should not fill this in and expect good things - do not remove this or risk form bot signups*/}
+						<div style={{ position: "absolute", left: "-5000px" }} aria-hidden="true">
+							<input
+								type="text"
+								name="b_71a58a6e738704a94ffc2ab89_cfcab31bbd"
+								tabIndex={-1}
+								defaultValue
+							/>
+						</div>
+						<div className="clear ">
+							<input
+								type="submit"
+								defaultValue="Subscribe"
+								name="subscribe"
+								id="mc-embedded-subscribe"
+								className="button registrate__submit"
+								value="Notificame!"
+							/>
+						</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	);

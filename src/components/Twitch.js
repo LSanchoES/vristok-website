e5b97e049d2160f7a784ea26f58bd3a0a7028fe6
vristok <lsanchoaragon@gmail.com>
2021-07-07 15:23:30 +0200

    Vercel Deploy again

--- src/components/Twitch.js
+++ src/components/Twitch.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export const Twitch = () => {
-    return (
-        <div id= "twitch-index" className="twitch__container"> 
-           
-            <h1> Directos diarios! </h1>
-            <iframe src="https://player.twitch.tv/?channel=vristok&parent=https://vristok-website.vercel.app//"
+	return (
+		<div id="twitch-index" className="twitch__container">
+			<h1> Directos diarios! </h1>
+
+			{/* <iframe src="https://player.twitch.tv/?channel=vristok&parent=https://vristok-website.vercel.app//"
             frameBorder="5" 
             allowFullScreen={true}
             scrolling="no"
             title="twitch"
-            ></iframe>
-        </div>
-    )
-}
+            ></iframe> */}
+
+			<iframe
+				src="https://player.twitch.tv/?channel=vristok&parent=vristok-website.vercel.app"
+				frameBorder="0"
+				allowFullScreen={true}
+				scrolling="no"
+				height="378"
+				width="620"
+                title="twitch"
+			></iframe>
+		</div>
+	);
+};
